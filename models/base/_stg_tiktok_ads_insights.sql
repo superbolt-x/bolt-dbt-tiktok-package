789f4638cda2de6183ed9a87eae23b11b3faff10
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-02-09 10:51:34 +0100

    Update _stg_tiktok_ads_insights.sql

--- models/base/_stg_tiktok_ads_insights.sql
+++ models/base/_stg_tiktok_ads_insights.sql
@@ -3,12 +3,80 @@
         unique_key='unique_key'
 ) }}
 
-{%- set schema_name, table_name = 'tiktok_raw', 'ad_report_daily' -%}
+{%- set schema_name, table_name = 'supermetrics_raw', 'ad_report_daily' -%}
 
 {%- set exclude_fields = [
-   "ctr",
-   "cpm",
-   "cpc"
+   "ad_group_id",
+   "ad_group_name",
+   "ad_group_opt_status",
+   "ad_group_status",
+   "ad_name",
+   "ad_opt_status",
+   "ad_status",
+   "ad_text",
+   "adgroup_placement",
+   "advertiser_address",
+   "advertiser_balance",
+   "advertiser_company",
+   "advertiser_country",
+   "advertiser_currency",
+   "advertiser_email",
+   "advertiser_id",
+   "advertiser_license_number",
+   "advertiser_name",
+   "advertiser_phone_number",
+   "advertiser_status",
+   "advertiser_timezone",
+   "app_download_url",
+   "app_id",
+   "app_name",
+   "app_package",
+   "app_type",
+   "audience_rule",
+   "audience_type",
+   "bid",
+   "bid_type",
+   "billing_method",
+   "call_to_action",
+   "campaign_budget",
+   "campaign_budget_mode",
+   "campaign_creation_date",
+   "campaign_id",
+   "campaign_modify_date",
+   "campaign_name",
+   "campaign_objective_type",
+   "campaign_operation_status",
+   "conversion_bid",
+   "cpv_video_duration",
+   "data_source_name",
+   "deep_bid_type",
+   "deep_cpabid",
+   "deep_external_action",
+   "display_name",
+   "external_action",
+   "image_mode",
+   "is_comment_disable",
+   "keywords",
+   "landing_page_url",
+   "optimize_goal",
+   "pacing",
+   "pixel_id",
+   "placement_type",
+   "playable_url",
+   "profile_image",
+   "schedule_end_time",
+   "schedule_start_time",
+   "schedule_type",
+   "statistic_type",
+   "target_age",
+   "target_android_osv",
+   "target_connection_type",
+   "target_device_price",
+   "target_gender",
+   "target_ios_osv",
+   "target_languages",
+   "target_operation_system",
+   "video_id"
 ]
 -%}
 
@@ -27,7 +95,7 @@ WITH insights AS
     )
 
 SELECT *,
-    MAX(_fivetran_synced) over () as last_updated,
+    MAX(date) over () as last_updated,
     ad_id||'_'||date as unique_key
 FROM insights
 {% if is_incremental() -%}
