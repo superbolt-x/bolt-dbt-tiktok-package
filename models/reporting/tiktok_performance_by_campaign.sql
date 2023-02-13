791c688300e7a0e1f843ebf0d03d56466ffef992
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-02-13 19:05:41 +0100

    Create tiktok_performance_by_campaign.sql

---
+++ models/reporting/tiktok_performance_by_campaign.sql
@@ -0,0 +1,49 @@
+{{ config (
+    alias = target.database + '_tiktok_performance_by_campaign'
+)}}
+
+{%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
+{%- set exclude_fields = ['date','day','week','month','quarter','year','last_updated','unique_key'] -%}
+{%- set dimensions = ['campaign_id'] -%}
+{%- set measures = adapter.get_columns_in_relation(ref('tiktok_campaigns_insights'))
+                    |map(attribute="name")
+                    |reject("in",exclude_fields)
+                    |reject("in",dimensions)
+                    |list
+                    -%}  
+
+WITH 
+    {%- for date_granularity in date_granularity_list %}
+
+    performance_{{date_granularity}} AS 
+    (SELECT 
+        '{{date_granularity}}' as date_granularity,
+        {{date_granularity}} as date,
+        {%- for dimension in dimensions %}
+        {{ dimension }},
+        {%-  endfor %}
+        {% for measure in measures -%}
+        COALESCE(SUM("{{ measure }}"),0) as "{{ measure }}"
+        {%- if not loop.last %},{%- endif %}
+        {% endfor %}
+    FROM {{ ref('tiktok_campaigns_insights') }}
+    GROUP BY {{ range(1, dimensions|length +2 +1)|list|join(',') }}),
+    {%- endfor %}
+
+    campaigns AS 
+    (SELECT campaign_id::VARCHAR as campaign_id, campaign_name, campaign_status
+    FROM {{ ref('tiktok_campaigns') }}
+    )
+
+SELECT *,
+    {{ get_tiktok_default_campaign_types('campaign_name')}}
+FROM 
+    ({% for date_granularity in date_granularity_list -%}
+    SELECT *
+    FROM performance_{{date_granularity}}
+    {% if not loop.last %}UNION ALL
+    {% endif %}
+
+    {%- endfor %}
+    )
+LEFT JOIN campaigns USING(campaign_id)
