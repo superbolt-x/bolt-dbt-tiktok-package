df0d48e126c42e6e291f2473ff6574c13d21c0d6
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-02-13 18:58:16 +0100

    Create _stg_tiktok_campaigns_insights.sql

---
+++ models/base/_stg_tiktok_campaigns_insights.sql
@@ -0,0 +1,50 @@
+{{ config( 
+        materialized='incremental',
+        unique_key='unique_key'
+) }}
+
+{%- set schema_name, table_name = 'supermetrics_raw', 'campaign_report_daily' -%}
+
+{%- set exclude_fields = [
+   "advertiser_address",
+   "advertiser_balance",
+   "advertiser_company",
+   "advertiser_country",
+   "advertiser_currency",
+   "advertiser_email",
+   "advertiser_id",
+   "advertiser_license_number",
+   "advertiser_name",
+   "advertiser_phone_number",
+   "advertiser_status",
+   "advertiser_timezone",
+   "campaign_budget",
+   "campaign_creation_date",
+   "campaign_name"
+]
+-%}
+
+{%- set fields = adapter.get_columns_in_relation(source(schema_name, table_name))
+                    |map(attribute="name")
+                    |reject("in",exclude_fields)
+                    -%}  
+
+WITH insights AS 
+    (SELECT 
+        {%- for field in fields if ("cost_per" not in field and "_rate" not in field) %}
+        {{ get_tiktok_clean_field(table_name, field) }}
+        {%- if not loop.last %},{%- endif %}
+        {%- endfor %}
+    FROM {{ source(schema_name, table_name) }}
+    )
+
+SELECT *,
+    MAX(date) over () as last_updated,
+    campaign_id||'_'||date as unique_key
+FROM insights
+{% if is_incremental() -%}
+
+  -- this filter will only be applied on an incremental run
+where date >= (select max(date)-30 from {{ this }})
+
+{% endif %}
