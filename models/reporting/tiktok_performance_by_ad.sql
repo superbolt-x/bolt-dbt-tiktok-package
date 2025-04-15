ccd0ea03b9db65553fd380b2a882bac4a7bbc535
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-04-15 17:57:18 +0200

    Update tiktok_performance_by_ad.sql

--- models/reporting/tiktok_performance_by_ad.sql
+++ models/reporting/tiktok_performance_by_ad.sql
@@ -3,7 +3,7 @@
 )}}
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
-{%- set exclude_fields = ['date','day','week','month','quarter','year','last_updated','unique_key'] -%}
+{%- set exclude_fields = ['date','day','week','month','quarter','year','last_updated','unique_key','secondaty_goal_result'] -%}
 {%- set dimensions = ['ad_id'] -%}
 {%- set measures = adapter.get_columns_in_relation(ref('tiktok_ads_insights'))
                     |map(attribute="name")
